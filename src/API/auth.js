a4e020c0d77e45df82636b892727945724404ec1
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2025-08-06 12:00:04 +0530

    Update auth.js

--- src/API/auth.js
+++ src/API/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const baseURL = 'http://localhost:8080';
+const baseURL ="/api";
+//const baseURL = 'http://localhost:8080';
 
 // Create a clean axios instance for authentication (without JWT token)
 const authAxios = axios.create({
